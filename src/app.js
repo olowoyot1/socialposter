8c9f4285d4b325114239735e26ff38a9e83c71a2
olowoyot1 <135076953+olowoyot1@users.noreply.github.com>
2026-07-14 07:21:31 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 import { accountsRouter } from "./routes/accounts.js";
 import { postsRouter } from "./routes/posts.js";
 import { cronRouter } from "./routes/cron.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.use("/accounts", accountsRouter);
 app.use("/posts", postsRouter);
